acb33c11170ddcb8f5a0c439037985b7d641e717
Patricia Moncada <cherrypsmr@gmail.com>
2023-09-14 22:48:06 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import { Route, Routes } from "react-router-dom";
+import Form from './Components/Form';
+import People from './Components/People';
+import Planets from "./Components/Planets";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Form></Form>
+      <Routes>
+        <Route path="/people/:id" element={<People></People>}></Route>
+        <Route path="/planets/:id" element={<Planets></Planets>}></Route>
+      </Routes>
     </div>
   );
 }
